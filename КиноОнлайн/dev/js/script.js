08c9fd928518cbc1fe4f0ac1fc7f6ff1efd8c9d1
Vetal <stekolschikov@mail.ua>
2016-08-25 19:50:44 +0300

    +slider

---
+++ КиноОнлайн/dev/js/script.js
@@ -0,0 +1,25 @@
+// slide
+$( document ).ready(function () {
+
+    var ALLSLIDES = $(".slide").length;
+    var NOWSLIDE = 1;
+
+    function nextSlide() {
+       if (ALLSLIDES != NOWSLIDE) {
+           $('#slider').animate({scrollLeft: '+=' + $( '.slide' ).width() }, 500);
+           NOWSLIDE += 1;
+       } else {
+           $('#slider').animate({scrollLeft: 0 }, 500);
+           NOWSLIDE = 1;
+       }
+       setTimeout(function(){nextSlide()}, 5000);
+    }
+    setTimeout(function(){nextSlide()}, 5000);
+
+    $( window ).resize(function() {
+        NOWSLIDE = 1;
+        $('#slider').animate({scrollLeft: 0 }, 500);
+    });
+
+});
+// slide //
